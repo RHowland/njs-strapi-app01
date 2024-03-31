cd9c4383921fad7e5531681c3ffb65a444619ace
Elias Emon <eliasemon0025@gmail.com>
2024-04-01 01:57:59 +0600

    Feature/header (#2)
    * feat(ui): create brand component for displaying brand logos/images
    
    This commit introduces the Brand component, designed to offer a reusable and standardized approach to showcasing brand logos or images across the application. The component accepts a src prop for specifying the image URL and an alt prop for alternative text. If the src prop is not provided, the component defaults to displaying a local logo image ("/Logo.png").
    
    * feat(ui): add header component for displaying navigation links and brand logo
    
    This commit introduces the Header component, which is responsible for rendering the website header with navigation links and the brand logo. The component is designed to be flexible and customizable, allowing developers to pass the brand props to the Brand component and define navigation links dynamically.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+
+import { NextPage } from "next";
+
+
+
+const Home : NextPage = () => {
   return (
     <div>
-      <h1>This is simple-site home page</h1>
+      <h1>This is simple site</h1>
     </div>
   );
 }
+export default Home;
