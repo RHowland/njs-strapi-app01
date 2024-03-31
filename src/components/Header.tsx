a3ff3d9159c5c120720af83ab9647b2a2cd61ec5
Elias Emon <eliasemon0025@gmail.com>
2024-04-01 02:11:53 +0600

    docs(header): add documentation for header component (#3)
    Added documentation to the Header component file (Header.tsx) including descriptions for the component's purpose, functionality, and input/output. Also included comments for section 1 defining the INavLink interface and initializing the navLinks array.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+// Component Name: Header
+
 import React,{ FC }  from "react";
 import Brand, { IBrandProps } from "@/components/ui/Brand";
 import Link from "next/link";
@@ -48,3 +50,42 @@ const Header: FC< Partial<IHeaderProps>> = ({brand}) => {
 };
 
 export default Header;
+
+
+/**
+ * ---------------------------------------------------------------------
+ * File Name      : Header.tsx
+ * Component Name : Header
+ * Component Type : ui
+ * Date Created   : 2024-03-31
+ * Dev Initials   : Elias Emon
+ * ------------------------------
+ * Question: Why was it necessary to create this component?
+ * Answer  : This component was necessary to establish a standardized header layout for the application, incorporating a brand logo and navigation links.
+ *
+ * ------------------------------
+ * Question: What does this component do?:
+ * Answer  :  The Header component renders the application's header section. It includes a brand logo displayed using the Brand component, passed through the
+ * brand prop. Additionally, it renders navigation links defined in the navLinks array, allowing users to navigate to different sections of the website.
+ *
+ * ------------------------------
+ * Section Comments:   (Enter "none" if you have no comments)
+ * Section 1:
+ * Defines the interface INavLink to represent the structure of each navigation link object, containing name and url properties.
+ * Also initializes the navLinks array with objects conforming to the INavLink interface. This array holds the navigation links used in the header component.
+ * 
+ * Section 2: Renders the navigation links by mapping over the navLinks array. Each link is wrapped in a Link component from Next.js, with its href attribute
+ * set to the corresponding URL.
+ * 
+ * ------------------------------
+ * Input Comments:     (Enter "none" if you have no comments)
+ * None
+ * ------------------------------
+ * Output Comments:    (Enter "none" if you have no comments)
+ * None
+ * ------------------------------
+ * Additional Comments:
+ * Question: Did you have to write any unusual code?
+ * Answer  : NO.
+ *
+ */
