23d2ee00d7204b7b8646477fcf1aac8918096000
Elias Emon <eliasemon0025@gmail.com>
2024-04-01 06:43:21 +0600

    feat(component): add Banner component (#5)
    Added Banner component (Banner.tsx) responsible for displaying customizable banners with dynamic content and images. Included props for banner heading, paragraph, and image, with default values if not provided. Documented the component's purpose, functionality, and code sections.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 
 import { NextPage } from "next";
+import Banner from '@/components/Banner';
 
 
 
 const Home : NextPage = () => {
   return (
     <div>
-      <h1>This is simple site</h1>
+      <Banner />
     </div>
   );
 }
